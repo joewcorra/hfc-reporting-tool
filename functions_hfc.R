6187abfaed71eb77c6b6b1a47540a5bc23543e78
joewcorra <corra.joseph@epa.gov>
2026-01-13 08:41:22 -0500

    Created scoping statement code; fixed EDGAR data in {pins}

--- functions_hfc.R
+++ functions_hfc.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(pins)
+library(gt)
 
 hfc_board <- board_folder("data/pins")
 
@@ -8,6 +9,7 @@ edgar <- pin_read(hfc_board, "edgar")
 velders <- pin_read(hfc_board, "velders")
 uncertainty <- pin_read(hfc_board, "uncertainty")
 mixture_compositions <- pin_read(hfc_board, "mixture_compositions")
+applications <- pin_read(hfc_board, "applications")
 
 # Tabs 3A, 3B, 3C----------------------------------
 
@@ -27,6 +29,28 @@ total_hfc <- user_input %>%
     
 # Tab Emissions Scoping Statement------------------------------
 
+user_input <- "usa"
 
+scoping <- edgar %>%
+  left_join(applications, by = "sub_application") %>%
+  filter(c_group == user_input) %>%
+  mutate(co2_eq = gwp * hfc_emissions) %>%
+  group_by(year, application, sub_application) %>%
+  summarize(mmt_co2_eq = sum(co2_eq, na.rm = TRUE) / 10^6) %>%
+  ungroup() 
 
+ scoping %>%
+   pivot_wider(names_from = year, values_from = mmt_co2_eq) %>%
+   gt()
+ # next: make this table pretty; get subtotals by application & annual totals
+ 
+ 
+ # Figures: Line plot (application~year); pie (current year by application); 
+ # pie (current year by subapplication)
+ 
+ 
+ # Refrigeration ----------------------------------
+ 
+ 
+  
 
