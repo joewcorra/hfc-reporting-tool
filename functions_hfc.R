b45d0aaa96b470f0c99cffff624ea24412b5affc
joewcorra <corra.joseph@epa.gov>
2026-01-13 16:53:58 -0500

    began refrigeration code

--- functions_hfc.R
+++ functions_hfc.R
@@ -48,9 +48,76 @@ scoping <- edgar %>%
  # Figures: Line plot (application~year); pie (current year by application); 
  # pie (current year by subapplication)
  
+ # Emission Model Assumptions---------------------------------
  
- # Refrigeration ----------------------------------
+ # Need to review all of these. Some are divided by the Velders emissions 
+ # factors, and some are not. Some applications are aggregated into what seem to be 
+ # arbitrary groups; others are unchanged from the scoping statement data. 
+ # need to add all missing apps to velders_applications with factor = 1.
  
+ abs_hfc_consumption <- scoping %>%
+   mutate(velders_application = case_when(
+     sub_application == "domestic refrigeration" ~ "domestic refrigeration", 
+     sub_application %in% c("commercial refrigeration",
+                            "industrial refrigeration") ~ "commercial and industrial refrigeration",
+     sub_application == "mobile air conditioning" ~ "mobile air conditioning",
+     sub_application == "stationary air conditioning" ~ "stationary air conditioning",
+     sub_application == "aerosols" ~ "aerosols",
+     sub_application == "closed cell foam" ~ "closed cell foam",
+     sub_application == "open cell foam" ~ "open cell foam",
+     sub_application %in% c("solvents", "f-gas as solvent") ~ "solvents",
+     sub_application %in% c("semiconductor/electronics manufacture", 
+                            "production of halocarbons and sf6", 
+                            "electrical equipment", 
+                            "production of metals") ~ "manufacturing and production",
+     sub_application == "fire extinguishers" ~ "fire extinguishers",
+     sub_application %in% c("other f-gas use", "other ods") ~ "other")
+   ) %>%
+   left_join(velders, by = "velders_application") %>%
+   group_by(year, velders_application) %>%
+   summarize(consumption_by_sector = sum(mmt_co2_eq / 
+                                           emission_factor_dev_countries, 
+                                         na.rm = TRUE))
  
+   
   
-
+# Refrigeration------------------------------------------------
+ 
+ # Why is the current year computed separately?
+ current_year <- 
+   # data from 'refrigeration_data_summary':
+   year = 
+   hfc = 
+   production = 
+   imports = 
+   exports = 
+   year_of_introduction = 
+   eq_sales_growth_rate =
+   eq_lifetime = 
+   installed_base_emission_factor = 
+   percent_hfc_destroyed_eol = 
+ 
+   # This is a mess--need to clarify what all of these variables arefore and why
+   # they are needed. 
+  
+ previous_years_estimated <-
+   #
+   year = 
+   hfc = 
+   production = 
+   agent_in_imports = 
+   agent_in_exports = 
+   total_new_agent = production + agent_in_imports + agent_in_exports
+   substance_in_eq_eol = 
+   destruction_in_eq_eol = substance_in_eq_eol * recovery
+   release_of_substabce_in_eq_eol = substance_in_eq_eol - destruction_in_eq_eol
+   bank = total_new_agent
+   emissions = bank * ef + release_of_substabce_in_eq_eol
+   uncertainty_upper_bound = 
+     uncertainty_lower_bound = 
+  emission_from_bank = bank * ef
+   emission_eq_eol = 
+     sum = emission_from_bank + emission_eq_eol
+   bankemissions = bank / emissions
+   
+   
