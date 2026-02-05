0deb6388f304a562826d8b6eb3693c8927f07ef2
joewcorra <corra.joseph@epa.gov>
2026-02-05 16:13:33 -0500

    Continued fiddling with refrigeration calcs (recursive bank formulas).

--- functions_hfc.R
+++ functions_hfc.R
@@ -78,15 +78,18 @@ scoping <- edgar %>%
  # Calculate net imports, exports, and production & destruction
 refrigeration_imports <- imports_data %>% 
    group_by(year, component) %>%
-   summarize(imports = new + recovered - feedstock)
+   summarize(imports = new + recovered - feedstock) %>%
+   ungroup()
  
  refrigeration_exports <- exports_data %>% 
    group_by(year, component) %>%
-   summarize(exports = new + recovered)
+   summarize(exports = new + recovered) %>%
+   ungroup()
  
  refrigeration_prod <- prod_data %>% 
    group_by(year, component) %>%
-   summarize(prod = produced - feedstock_produced - destroyed)
+   summarize(prod = produced - feedstock_produced - destroyed) %>%
+   ungroup()
  
  # Join all refrigeration data
  refrigeration <- list(
@@ -95,28 +98,54 @@ refrigeration_imports <- imports_data %>%
    refrigeration_prod) %>%
    reduce(full_join, by = c("year", "component")) %>%
    left_join(hfc_defaults, by = c("component" = "hfc")) %>%
+   mutate(bank_start_of_year = 0, 
+          bank_end_of_year = 0) %>%
+   add_row(year = as_factor(2019), 
+           bank_start_of_year = 0, 
+           bank_end_of_year = 0) %>%
+   
    mutate(
-     ef_filling = emission_factor_filling,
+     ef_filling = 0,
      ef_use = emission_factor_installed_base,
-     bank_end_of_year = 0, # default value to prevent error. redefined below.
-     bank_start_of_year =  lag(bank_end_of_year, n = 1, order_by = year, default = 0),
+     # new_equip_imports and new_equip_exports need to be made user input values
+     new_equip_imports = 0, 
+     new_equip_exports = 0, 
      domestic_sales = imports + prod - exports, 
-     in_use_equip_emitted = bank_start_of_year * ef_use, 
-     servicing = pmax(domestic_sales, in_use_equip_emitted, na.rm = TRUE), 
+     bank_start_of_year = lag(bank_end_of_year, 
+                               n = 1,
+                               order_by = year),
+     in_use_equip_emissions = bank_start_of_year * ef_use, 
+     servicing = pmax(domestic_sales, 
+                      in_use_equip_emissions, na.rm = TRUE), 
      new_equip_filling = domestic_sales - servicing,
      filling_emissions = new_equip_filling * ef_filling, 
      new_equip_contained = new_equip_filling - filling_emissions,
+
      bank_contained = new_equip_contained + new_equip_imports - new_equip_exports,
-     retired_equip = lag(bank_contained, n = 10, order_by = year, default = 0),
+     retired_equip = 0,
+       # lag(bank_contained, 
+       #                   n = 10, 
+       #                   order_by = year, 
+       #                   default = 0),
      reclaimed = retired_equip * destruction_at_end_of_life,
-     bank_end_of_year = bank_start_of_year + bank_contained + servicing - in_use_equip_emitted - retired_equip)
- 
- abs_hfc_consumption %>%
-   left_join(uncertainty) %>%
+     # Destroyed and exported in used equipment need to be made user input values
+     destroyed = 0, 
+     exported_used_equip = 0, 
+     end_of_life_emissions = retired_equip - reclaimed - destroyed - exported_used_equip, 
+     bank_end_of_year = bank_start_of_year + bank_contained + servicing - in_use_equip_emissions - retired_equip, 
+     total_emissions = filling_emissions + in_use_equip_emissions + end_of_life_emissions) %>%
+   left_join(abs_hfc_consumption %>% 
+               filter(meta_application == "refrigeration and air conditioning") %>%
+               select(-meta_application),
+             by = "year") %>%
+   left_join(uncertainty %>%
+               filter(parameter == "kigali data") %>%
+               select(-parameter) %>%
+               mutate(hfc = str_remove_all(hfc, "-")), 
+             by = c("component" = "hfc")) %>%
    mutate(
-     # IS THIS RIGHT????????????????????????????
-     lower_estimate = consumption_share * (1 - uncertainty),
-     upper_estimate = consumption_share * (1 + uncertainty))
+     lower_estimate = total_emissions * consumption_share * (1 - uncertainty),
+     upper_estimate = total_emissions * consumption_share * (1 + uncertainty))
  
  
 
